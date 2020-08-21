0acb06c37349ffab1c08dcaac8332c1606c8a290
Spencer Moody <spencer.moody@outlook.com>
2020-08-21 12:25:35 -0700

    Add code to UsersController and new Migrations

---
+++ AnimalShelter/Migrations/20200821191953_NewUserRole.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class NewUserRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName", "Password", "Role", "Token", "Username" },
+                values: new object[] { 1, "Admin", "Admin", "Admin", "Admin", null, "Admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
